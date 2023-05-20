426f6dfce988c05afe9903ba10b4a4cf03a2879f
zbz <nnzbz@21cn.com>
2023-05-20 20:52:41 +0800

    + 添加ant-svr项目

---
+++ ant-svr/src/main/java/myvertx/ant/verticle/MainVerticle.java
@@ -0,0 +1,37 @@
+package myvertx.gatex.verticle;
+
+import com.google.inject.AbstractModule;
+import com.google.inject.Module;
+import io.vertx.core.Verticle;
+import lombok.extern.slf4j.Slf4j;
+import myvertx.gatex.inject.MainModule;
+import rebue.wheel.vertx.verticle.AbstractMainVerticle;
+
+import java.util.List;
+import java.util.Map;
+import java.util.ServiceLoader;
+
+@Slf4j
+public class MainVerticle extends AbstractMainVerticle {
+
+    /**
+     * 添加guice模块
+     *
+     * @param guiceModules 添加guice模块到此列表
+     */
+    @Override
+    protected void addGuiceModules(final List<Module> guiceModules) {
+        guiceModules.add(new MainModule());
+    }
+
+    /**
+     * 添加要部署的Verticle类列表
+     *
+     * @param verticleClasses 添加Verticle类到此列表
+     */
+    @Override
+    protected void addVerticleClasses(final Map<String, Class<? extends Verticle>> verticleClasses) {
+        verticleClasses.put("web", WebVerticle.class);
+    }
+
+}
