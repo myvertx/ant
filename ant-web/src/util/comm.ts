8b4eb3bba2c6873e1215710add5db81f9d53a55f
zbz <nnzbz@21cn.com>
2023-05-24 20:12:57 +0800

    * 字节转换的另一种算法

--- ant-web/src/util/comm.ts
+++ ant-web/src/util/comm.ts
@@ -1,16 +1,8 @@
-export function byteConvert(bytes: number) {
-    if (isNaN(bytes)) {
-        return '';
-    }
-    let symbols = ['bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
-    let exp = Math.floor(Math.log(bytes) / Math.log(2));
-    if (exp < 1) {
-        exp = 0;
-    }
-    let i = Math.floor(exp / 10);
-    bytes = bytes / Math.pow(2, 10 * i);
-
-    const bytesStr = bytes.toString().length > bytes.toFixed(2).toString().length ? bytes.toFixed(2) : bytes;
-
-    return bytesStr + ' ' + symbols[i];
+/** 字节转换 */
+export function byteConvert(size: number) {
+    const units = ['B', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
+    size = Math.abs(size);
+    let i = 0;
+    for (; size >= 1024 && i < 4; i++) size /= 1024;
+    return (size < 0 ? '-' : '') + size.toFixed(2) + units[i];
 }
